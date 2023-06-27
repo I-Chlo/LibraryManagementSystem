d0e070749da5d549e81f71fd949349bcbf1889ab
Chloe Hazell <9887505+I-Chlo@users.noreply.github.com>
2023-06-27 19:54:03 +0100

    Refacted the verifyBookPOSTData method and moved ISBN verification into its own method so that it can be used by other function
    Also cleaned up the verifyBookPOSTData method so that each checking statement follows the same routine design

--- Backend/MySQL/SQLErrorChecking.py
+++ Backend/MySQL/SQLErrorChecking.py
@@ -28,19 +28,38 @@ def _verifyBookEDITION(edition):
         return True
 
 def _verifyBookDATE(date):
-    date = datetime.datetime.strptime("2023-06-25", "%Y-%m-%d")
-
+    try:
+        date = datetime.datetime.strptime("2023-06-25", "%Y-%m-%d")
+        return True
+    except Exception as e:
+        return Response("Book DATE is in the wrong format: ERR: " + e, status=400)
+def _verifyBookISBN(isbn):
+    try:
+        int(isbn)
+        if len(isbn) == 13:
+            # Return if both criteria are satified
+            return True
+        else:
+            # Return if the ISBN isn't exactly 13 character long
+            return Response("ISBN Wrong length. L", status=400)
+    except Exception as e:
+        # Return if the ISBN is not an integer value
+        return Response("ISBN invalid <- Only integers allowed in ISBN.\nException: " + str(e), status=400)
 
 def verifyBookPOSTData(data):
     if data.__len__() != 0:
 
         # We now need to check that the data at least contains an ISBN, that it converts into a valid number and is 13 characters long
-        if data['ISBN']:
-            try:
-                int(data['ISBN'])
-                if len(data['ISBN']) == 13:
-                    # Check that the other member variables for the book attribute are present and correct
+        if not data['ISBN']:
+            # ISBN doesn't exist <- we need to inform the user
+            return Response("No ISBN = No Bueno", status=400)
+        else:
+                if isinstance(bookISBNVerifyReturn := _verifyBookISBN(data['ISBN']), Response):
+                    # Return if the ISBN isn't valid
+                    return bookISBNVerifyReturn
 
+                else:
+                    # The ISBN is valid so we can continue
                     # CHECK TITLE DATA
                     if not data['TITLE']:
                         # No TITLE has been found in the data
@@ -50,44 +69,36 @@ def verifyBookPOSTData(data):
                             # TITLE data is good so we can continue
                             return bookTitleVerifyReturn
 
-                    # CHECK AUTHOR DATA
+                        # CHECK AUTHOR DATA
                     if not data['AUTHOR']:
-                        # No AUTHOR has been found in the data
-                        return Response("Book AUTHOR not found", status=400)
+                            # No AUTHOR has been found in the data
+                            return Response("Book AUTHOR not found", status=400)
                     else:
-                        if isinstance(bookAuthorVerifyReturn := _verifyBookAUTHOR(data['AUTHOR']), Response):
-                            # AUTHOR data is good so we can continue
-                            return bookAuthorVerifyReturn
+                            if isinstance(bookAuthorVerifyReturn := _verifyBookAUTHOR(data['AUTHOR']), Response):
+                                # AUTHOR data is good so we can continue
+                                return bookAuthorVerifyReturn
 
-                    # CHECK EDITION DATA
+                        # CHECK EDITION DATA
                     if not data['EDITION']:
-                        # No EDITION has been found in the data
-                        return Response("Book EDITION not found", status=400)
+                            # No EDITION has been found in the data
+                            return Response("Book EDITION not found", status=400)
                     else:
-                        if isinstance(bookEditionVerifyReturn := _verifyBookAUTHOR(data['EDITION']), Response):
-                            # EDITION data is good so we can continue
-                            return bookEditionVerifyReturn
+                            if isinstance(bookEditionVerifyReturn := _verifyBookAUTHOR(data['EDITION']), Response):
+                                # EDITION data is good so we can continue
+                                return bookEditionVerifyReturn
 
-                    # CHECK DATE DATA
+                        # CHECK DATE DATA
                     if not data['DATE']:
-                        # No DATE has been found in the data
-                        return Response("Book DATE not found", status=400)
+                            # No DATE has been found in the data
+                            return Response("Book DATE not found", status=400)
                     else:
-                        if isinstance(bookDateVerifyReturn := _verifyBookDATE(data['DATE']), Response):
-                            # DATA data is good so we can continue
-                            return bookDateVerifyReturn
+                            if isinstance(bookDateVerifyReturn := _verifyBookDATE(data['DATE']), Response):
+                                # DATA data is good so we can continue
+                                return bookDateVerifyReturn
 
-                    # If we have reached here that means that all elements in the POST request where valid and can be entered into the database.
+                        # If we have reached here that means that all elements in the POST request where valid and can be entered into the database.
                     return True
-                else:
-                    # The ISBN wasn't the correct length - therefore we tell the use
-                    return Response("ISBN Wrong length. L", status=400)
-            except Exception as e:
-                # The ISBN cannot be converted into an int and is therefore not valid
-                return Response("ISBN invalid <- Only integers allowed in ISBN.\nException: " + str(e), status=400)
-        else:
-            # ISBN doesn't exist <- we need to inform the user
-            return Response("No ISBN = No Bueno", status=400)
+
 
     else:
         # Nothing was sent in the POST request, so we need to send an angry letter back to the user.
